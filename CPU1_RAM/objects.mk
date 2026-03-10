ad16b04c6e1905fbeac51c264f1e7f9988d33025
nkale <nkale@ridealso.com>
2026-03-10 15:25:43 -0700

    Add .gitignore and ignore build artifacts

--- CPU1_RAM/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -ldriverlib.lib -lc2000ware_libraries.cmd.genlibs -llibc.a
-
